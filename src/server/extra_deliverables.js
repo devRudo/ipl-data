d69a556720614909fc349f04dbdffc390c9a2c4f
VIJAY KUMAR MISHRA <vijay.mishra@mountblue.tech>
2020-02-21 18:33:22 +0530

    Add function for player per season having most player of the match

--- src/server/extra_deliverables.js
+++ src/server/extra_deliverables.js
@@ -11,7 +11,34 @@ module.exports.wonTossWonMatchPerTeam = function (obj) {
 }
 
 module.exports.playerHighManoftheMatchPerSeason = function (obj) {
-
+    let years = obj.map(elem => elem.season).filter((curr, index, arr) => arr.indexOf(curr) === index);
+    let resultobj = {};
+    for (let i = 0; i < years.length; i++) {
+        // console.log(years[i]);
+        let players = obj.filter(elem => elem.season === years[i]).map(elem => elem.player_of_match).reduce((acc, curr) => {
+            if (acc[curr] === undefined) {
+                acc[curr] = 1;
+            }
+            else {
+                acc[curr]++;
+            }
+            return acc;
+        }, {});
+        let countArr = [];
+        for (key in players) {
+            countArr.push(players[key]);
+        }
+        let highestCount = Math.max(...countArr);
+        // let result = [];
+        let player = {};
+        for (key in players) {
+            if (players[key] == highestCount) {
+                player[key] = highestCount;
+            }
+        }
+        resultobj[years[i]] = player;
+    }
+    return resultobj;
 }
 
 module.exports.strikeRateViratPerseason = function (matchObj, deliveryObj) {
