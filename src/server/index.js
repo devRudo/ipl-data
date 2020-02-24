1380a04ab255ef3af319b539beb896adbdf69564
VIJAY KUMAR MISHRA <vijay.mishra@mountblue.tech>
2020-02-24 11:32:06 +0530

    Update index.html - Error Handling in writing file - Format console messages

--- src/server/index.js
+++ src/server/index.js
@@ -7,63 +7,64 @@ let pwd = process.cwd();
 
 fs.writeFile(pwd + '/src/output/matchesPerYear.json', JSON.stringify(ipl.matchesPerYear(conversion.matchResult)) + '\n', function (err) {
     if (err) {
-        return console.error(err);
+        console.error("Opoops ! Something went wrong");
     }
-    console.log('File is created successfully.');
+    console.log('matchesPerYear.json is created successfully.');
 });
 
 fs.writeFile(pwd + '/src/output/matchesPerTeamPerYear.json', JSON.stringify(ipl.matchesPerTeamPerYear(conversion.matchResult)) + '\n', function (err) {
     if (err) {
-        return console.error(err);
+        console.error("Opoops ! Something went wrong");
     }
-    console.log('File is created successfully.');
+    console.log('matchesPerTeamPerYear.json is created successfully.');
 });
 
 fs.writeFile(pwd + '/src/output/wonTossWonMatchPerTeam.json', JSON.stringify(extra.wonTossWonMatchPerTeam(conversion.matchResult)) + '\n', function (err) {
     if (err) {
         return console.error(err);
     }
-    console.log('File is created successfully.');
+    console.log('wonTossWonMatchPerTeam.json is created successfully.');
 });
 
 fs.writeFile(pwd + '/src/output/playerHighManoftheMatchPerSeason.json', JSON.stringify(extra.playerHighManoftheMatchPerSeason(conversion.matchResult)) + '\n', function (err) {
     if (err) {
-        return console.error(err);
+        console.error("Opoops ! Something went wrong");
     }
-    console.log('File is created successfully.');
+    console.log('playerHighManoftheMatchPerSeason.json is created successfully.');
 });
 
 fs.writeFile(pwd + '/src/output/mostEconomicalBowlerSuperOver.json', JSON.stringify(extra.bowlerWithBestEconomyInSuperOvers(conversion.deliveryResult)) + '\n', function (err) {
     if (err) {
-        return console.error(err);
+        console.error("Opoops ! Something went wrong");
     }
-    console.log('File is created successfully.');
+    console.log('mostEconomicalBowlerSuperOver.json is created successfully.');
 });
 
 fs.writeFile(pwd + '/src/output/strikeRateViratPerseason.json', JSON.stringify(extra.strikeRateViratPerseason(conversion.matchResult, conversion.deliveryResult)) + '\n', function (err) {
     if (err) {
-        return console.error(err);
+        console.error("Opoops ! Something went wrong");
     }
-    console.log('File is created successfully.');
+    console.log('strikeRateViratPerseason.json is created successfully.');
 });
 
 fs.writeFile(pwd + '/src/output/extraRunsPerTeam.json', JSON.stringify(ipl.extraRunsPerTeam(conversion.matchResult, conversion.deliveryResult, '2016')) + '\n', function (err) {
     if (err) {
-        return console.error(err);
+        console.error("Opoops ! Something went wrong");
     }
-    console.log('File is created successfully.');
+    console.log('extraRunsPerTeam.json is created successfully.');
 });
 
 fs.writeFile(pwd + '/src/output/topEconomicalBowlers.json', JSON.stringify(ipl.topEconomicalBowlers(conversion.matchResult, conversion.deliveryResult, '2015')) + '\n', function (err) {
     if (err) {
-        return console.error(err);
+        console.error("Opoops ! Something went wrong");
     }
-    console.log('File is created successfully.');
+    console.log('topEconomicalBowlers.json is created successfully.');
 });
 
 fs.writeFile(pwd + '/src/output/maxNumberofTimesofDissmissal.json', JSON.stringify(extra.playerDissmisal(conversion.deliveryResult)) + '\n', function (err) {
     if (err) {
-        return console.error(err);
+        console.error("Opoops ! Something went wrong");
     }
-    console.log('File is created successfully.');
+    console.log('maxNumberofTimesofDissmissal.json is created successfully.');
 });
+console.log("Hello");
